534347fc806700326d063a26a4d867997922a9cf
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2021-08-20 12:21:06 +0100

    Add description to prometheus_all variables

--- prometheus_all/input.tf
+++ prometheus_all/input.tf
@@ -1,25 +1,77 @@
-variable "monitoring_instance_name" {}
-variable "monitoring_org_name" {}
-variable "monitoring_space_name" {}
+variable "monitoring_instance_name" {
+  description = "Name used as suffix on all created resources to identify the deployment uniquely"
+}
+variable "monitoring_org_name" {
+  description = "Cloud foundry organisation where the monitoring solution is deployed"
+}
+variable "monitoring_space_name" {
+  description = "Cloud foundry space where the monitoring solution is deployed"
+}
 
-variable "paas_exporter_username" {}
-variable "paas_exporter_password" {}
+variable "paas_exporter_username" {
+  description = "Readonly cloud foundry user to query cloud foundry metrics. It must have BillingManager org role and SpaceAuditor space role on all the monitored spaces."
+  default     = ""
+}
+variable "paas_exporter_password" {
+  description = "Readonly cloud foundry user password"
+  default     = ""
+  sensitive   = true
+}
 
-variable "alertmanager_config" { default = "" }
-variable "alertmanager_slack_url" { default = "" }
-variable "alertmanager_slack_channel" { default = "" }
-variable "alert_rules" { default = "" }
+variable "alertmanager_config" {
+  description = "Alert manager configuration file (https://prometheus.io/docs/alerting/latest/configuration/). Passed as a long string."
+  default     = ""
+}
+variable "alertmanager_slack_url" {
+  description = "Slack webhook URL for alert notifications"
+  default     = ""
+}
+variable "alertmanager_slack_channel" {
+  description = "Slack channel for alert notifications"
+  default     = ""
+}
+variable "alert_rules" {
+  description = "Prometheus alert rules file (https://prometheus.io/docs/prometheus/latest/configuration/alerting_rules/). Passed as a long string."
+  default     = ""
+}
 
-variable "grafana_google_client_id" { default = "" }
-variable "grafana_google_client_secret" { default = "" }
-variable "grafana_admin_password" {}
-variable "grafana_json_dashboards" { default = [] }
-variable "grafana_extra_datasources" { default = [] }
-variable "grafana_google_jwt" { default = "" }
-variable "grafana_runtime_version" { default = "6.5.1" }
+variable "grafana_google_client_id" {
+  description = "Google client id for Grafana Google single-sign-on"
+  default     = ""
+}
+variable "grafana_google_client_secret" {
+  description = "Google client secret for Grafana Google single-sign-on"
+  default     = ""
+}
+variable "grafana_admin_password" {
+  description = "Grafana administrator password (User: admin)"
+}
+variable "grafana_json_dashboards" {
+  description = <<-DESCRIPTION
+  Additional Grafana dashboards. Passed as a list of long json strings.
+  The dashboard can be exported as json from Grafana UI: https://grafana.com/docs/grafana/latest/dashboards/export-import/"
+  DESCRIPTION
+  default     = []
+}
+variable "grafana_extra_datasources" {
+  description = <<-DESCRIPTION
+  Additional Grafana data sources. Passed as a list of long yaml strings.
+  See https://grafana.com/docs/grafana/latest/administration/provisioning/#data-sources
+  DESCRIPTION
+  default     = []
+}
+variable "grafana_google_jwt" {
+  description = "Google JWT token for Grafana Google sheet data source"
+  default     = ""
+}
+variable "grafana_runtime_version" {
+  description = "Override default Grafana version. See Grafana module for current default version."
+  default     = null
+}
 
 variable "grafana_elasticsearch_credentials" {
-  type = map(any)
+  description = "Credentials for Grafana Elasticserach datasource. Map of {url, username, password}."
+  type        = map(any)
 
   default = {
     url      = ""
@@ -28,19 +80,41 @@ variable "grafana_elasticsearch_credentials" {
   }
 }
 
-variable "prometheus_memory" { default = null }
-variable "prometheus_disk_quota" { default = null }
+variable "prometheus_memory" {
+  description = "Override default prometheus application allocated memory. See Prometheus module for current default value."
+  default     = null
+}
+variable "prometheus_disk_quota" {
+  description = "Override default prometheus application allocated disk space. See Prometheus module for current default value."
+  default     = null
+}
 
-variable "influxdb_service_plan" { default = "tiny-1_x" }
+variable "influxdb_service_plan" {
+  description = "Influxdb PaaS service plan. Run 'cf marketplace' to see available plans."
+  default     = "tiny-1_x"
+}
 
-variable "redis_services" { default = [] }
-variable "postgres_services" { default = [] }
+variable "redis_services" {
+  description = "List of redis services for advanced monitoring and dashboard. [\"space1/redis1\", \"space2/redis2\", ...]"
+  default     = []
+}
+variable "postgres_services" {
+  description = "List of postrgres services for advanced monitoring and dashboard. [\"space1/postgres1\", \"space2/postgres2\", ...]"
+  default     = []
+}
 
-variable "external_exporters" { default = [] }
-variable "internal_apps" { default = [] }
+variable "external_exporters" {
+  description = "List of extra Prometheus exporter domains. Prometheus will scrape each domain via https at /metrics path."
+  default     = []
+}
+variable "internal_apps" {
+  description = "List internal PaaS applications providing metrics. Prometheus will scrape each app instance at '/metrics' path"
+  default     = []
+}
 
 variable "enabled_modules" {
-  type = list(any)
+  description = "List of prometheus_all modules. Add them one by one to make onboarding easier."
+  type        = list(any)
   default = [
     "paas_prometheus_exporter",
     "prometheus",
