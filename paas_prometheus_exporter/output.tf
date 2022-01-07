0ce472a0fd6882890a833b619e19ea4394f9cbb0
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-01-07 15:39:28 +0000

    Use CF instance label from the paas exporter
    The CF app instance number is provided by the paas prometheus exporter
    but it conflicts with the prometheus own instance label, which we're
    not interested in.
    
    By using "honor_labels" we tell prometheus to replace its own label with
    the one from the exporter.
    
    This applies to all the exporters but only the paas one sends an
    instance label so other ones are not impacted.

--- paas_prometheus_exporter/output.tf
+++ paas_prometheus_exporter/output.tf
@@ -1,8 +1,8 @@
 output "exporter" {
   value = {
-    endpoint = cloudfoundry_route.paas_prometheus_exporter.endpoint
-    name     = cloudfoundry_app.paas_prometheus_exporter.name
-    scheme   = "https"
+    endpoint     = cloudfoundry_route.paas_prometheus_exporter.endpoint
+    name         = cloudfoundry_app.paas_prometheus_exporter.name
+    scheme       = "https"
+    honor_labels = true
   }
 }
-
