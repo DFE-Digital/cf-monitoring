f00446e4e8b9d7d8e5088486f8e6fb176492c2c8
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-01-17 12:31:12 +0000

    Bump default Grafana version to 7

--- grafana/input.tf
+++ grafana/input.tf
@@ -25,7 +25,7 @@ variable "json_dashboards" { default = [] }
 variable "extra_datasources" { default = [] }
 
 locals {
-  default_runtime_version = "6.5.1"
+  default_runtime_version = "7.5.12"
   dashboard_list          = fileset(path.module, "dashboards/*.json")
   dashboards              = [for f in local.dashboard_list : file("${path.module}/${f}")]
   grafana_ini_variables = {
